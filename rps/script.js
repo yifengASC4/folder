d4d779621420b6dc09738a8068d7f73e36c4d37c
yifengASC4 <yifengzheng5678@gmail.com>
2017-08-07 16:25:17 -0400

    rock paper scissors

---
+++ rps/script.js
@@ -0,0 +1,16 @@
+var userChoice = [];
+var compChoice = Math.random();
+var choices = ["rock", "paper", "scissor"];
+function myFunction(){
+    $('#picBackground img').attr('src', 'http://1000awesomethings.com/wp-content/uploads/2008/08/rock.jpg');
+    userChoice = "rock";
+    if (compChoice <= .34){
+        compChoice = rock
+    }
+}
+function paperImg(){
+    $('#picBackground img').attr('src', 'http://1000awesomethings.com/wp-content/uploads/2008/08/paper.jpg');
+}
+function scissorImg(){
+    $('#picBackground img').attr('src', 'http://4.bp.blogspot.com/-v_ooWUiEKNQ/UGOonq6f4-I/AAAAAAAACks/yUzYr7OBTug/s1600/scissors.jpg');
+}
