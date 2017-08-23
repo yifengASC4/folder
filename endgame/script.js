2638dccb9919412e849a511e5727daa64ae3077c
yifengASC4 <yifengzheng5678@gmail.com>
2017-08-23 12:25:25 -0400

    reset changes

--- endgame/script.js
+++ endgame/script.js
@@ -2,38 +2,35 @@ var ship;
 var asteroids = [];
 var lasers = [];
 var gameover = false;
-var mode;
-function setup(){
-    createCanvas(windowWidth - 10, windowHeight - 50);
+function setup() {
+    createCanvas(1349, 755);
     ship = new Ship();
-    mode = new GameMode(currentMode);
-    console.log(mode);
-    for (var j=0; j< mode.amountOfAsteroids; j++){
+    for (var j = 0; j < 15; j++) {
         asteroids.push(new Asteroid());
     }
-    
-}
-function draw(){
-    if (gameover == false){
-        background(0);
-        for (var i=0; i<asteroids.length; i++){
-            if (ship.hits(asteroids[i])){
-                gameover = true;
 
+}
+function draw() {
+    background(0);
+
+    if (gameover == false) {
+        for (var i = 0; i < asteroids.length; i++) {
+            if (ship.hits(asteroids[i])) {
+                gameover = true;
             }
             asteroids[i].render();
             asteroids[i].update();
             asteroids[i].edges();
         }
-        for (var i=lasers.length-1; i>=0; i--){
+        for (var i = lasers.length - 1; i >= 0; i--) {
             lasers[i].render();
             lasers[i].update();
-            if (lasers[i].offscreen()){
+            if (lasers[i].offscreen()) {
                 lasers.splice(i, 1);
-            }else{
-                for (var k=asteroids.length-1; k>=0; k--){
-                    if (lasers[i].hits(asteroids[k])){
-                        if (asteroids[k].r > 20){
+            } else {
+                for (var k = asteroids.length - 1; k >= 0; k--) {
+                    if (lasers[i].hits(asteroids[k])) {
+                        if (asteroids[k].r > 20) {
                             var newAsteroids = asteroids[k].breakup();
                             asteroids = asteroids.concat(newAsteroids);
                         }
@@ -49,40 +46,40 @@ function draw(){
         ship.turn();
         ship.update();
         ship.edges();
-    }else{
+    } else {
         fill(70, 130, 180);
         textSize(100);
-        text("GAME OVER", width/3.5, height/3);
+        text(" GAME OVER ", width / 3.5, height / 3);
         fill(70, 130, 180);
         textSize(60);
-        text("YOU LOSE TOO BAD =(!!", width/4, height/2);
+        text(" YOU LOSE TOO BAD =(!! ", width / 4, height / 2);
     }
-    if (asteroids.length == 0){
+    if (asteroids.length == 0) {
         fill(70, 130, 180);
         textSize(100);
-        text("GAME OVER", width/3.5, height/3);
+        text(" GAME OVER ", width / 3.5, height / 3);
         fill(70, 130, 180);
         textSize(50);
-        text("YOU WIN!!", width/2.5, height/2);
+        text(" YOU WIN!! ", width / 2.5, height / 2);
     }
-    
+
 }
 
-function keyReleased(){
+function keyReleased() {
     ship.setRotation(0);
     ship.boosting(false);
 }
-function keyPressed(){
-    if (key == ' '){
+function keyPressed() {
+    if (key == ' ') {
         lasers.push(new Laser(ship.pos, ship.heading));
-    }else if (keyCode == RIGHT_ARROW) {
+    } else if (keyCode == RIGHT_ARROW) {
         ship.setRotation(0.1);
-    }else if (keyCode == LEFT_ARROW) {
+    } else if (keyCode == LEFT_ARROW) {
         ship.setRotation(-0.1);
-    }else if (keyCode == UP_ARROW) {
+    } else if (keyCode == UP_ARROW) {
         ship.boosting(true);
     }
 }
-function reset(){
+function reset() {
     window.location.reload();
 }
